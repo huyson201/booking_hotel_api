615db40ff9d4ce42d1842de3f8c1cd569ce51fc6
HuySon <huyson201@gmail.com>
2021-11-02 07:27:51 +0700

    make it better

--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,10 @@ const userRoute = require("./user")
 const invoiceRoute = require("./invoice")
 const staffRoute = require("./staff")
 const authMiddleware = require('../middleware/auth')
-const { authRole } = require('../auth')
-const role = require('../config/role')
+
 const route = (app) => {
     app.use('/', siteRoute)
-    app.use('/hotels', authMiddleware.checkToken, authRole([role.ADMIN, role.OWNER, role.HOTEL_STAFF]), hotelRoute)
+    app.use('/hotels', hotelRoute)
     app.use('/rooms', roomRoute)
     app.use('/users', authMiddleware.checkToken, userRoute)
     app.use('/invoices', invoiceRoute)
