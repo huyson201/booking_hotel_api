3cc2ffd8d06403f867b308b63e29b89e69762704
kieuoanh231 <kieuoanh2631@gmail.com>
2021-11-29 20:57:15 +0700

    fix add room

--- controllers/room.js
+++ controllers/room.js
@@ -1,100 +1,126 @@
-const { Room } = require('../models')
-const { uploadFile } = require('../s3')
+const { Room } = require("../models");
+const { uploadFile } = require("../s3");
 class RoomController {
-    async index(req, res) {
-        try {
-            let rooms = await Room.findAll()
-            return res.json({ msg: "success", data: rooms })
-        }
-        catch (err) {
-            console.log(err)
-            return res.send(err)
-        }
+  async index(req, res) {
+    try {
+      let rooms = await Room.findAll();
+      return res.json({ msg: "success", data: rooms });
+    } catch (err) {
+      console.log(err);
+      return res.send(err);
     }
+  }
 
-    async getById(req, res) {
-        let id = req.params.id
-        if (!id) return res.status(404).json({ msg: "id not found!" })
-        try {
-            let room = await Room.findByPk(id)
-            return res.json({ msg: "success", data: room })
-        }
-        catch (err) {
-            console.log(err)
-            return res.send(err)
-        }
+  async getById(req, res) {
+    let id = req.params.id;
+    if (!id) return res.status(404).json({ msg: "id not found!" });
+    try {
+      let room = await Room.findByPk(id);
+      return res.json({ msg: "success", data: room });
+    } catch (err) {
+      console.log(err);
+      return res.send(err);
     }
+  }
 
-    async create(req, res) {
+  async create(req, res) {
+    let slideImgs = req.files;
+    if (!slideImgs)
+      return res.status(404).json({
+        code: 404,
+        name: "Not found",
+        message: "slide images required!",
+      });
+    let {
+      room_name,
+      room_price,
+      room_desc,
+      room_area,
+      room_bed,
+      room_num_people,
+      room_quantity,
+      hotel_id,
+      room_services,
+    } = req.body;
 
-        let slideImgs = req.files
-        if (!slideImgs) return res.status(404).json({ code: 404, name: "Not found", message: "slide images required!" })
-        let { room_name, room_price, room_desc, room_area, room_bed, room_num_people, room_quantity, hotel_id } = req.body
+    try {
+      // upload img
+      let roomImgsUrl = [];
+      for (let img of slideImgs) {
+        let result = await uploadFile(img);
+        roomImgsUrl.push(process.env.APP_BASE_URL + "/images/" + result.key);
+      }
 
-        try {
-            // upload img
-            let roomImgsUrl = []
-            for (let img of slideImgs) {
-                let result = await uploadFile(img)
-                roomImgsUrl.push(result.key)
-            }
-
-            let room = await Room.create({ room_name, hotel_id: +hotel_id, room_desc, room_area, room_price, room_bed: +room_bed, room_quantity: +room_quantity, room_num_people: +room_num_people, room_imgs: roomImgsUrl.join() })
-            return res.status(201).json({ code: 201, name: "Created", message: "create room successfully!", data: room })
-        } catch (error) {
-            console.log(error)
-            return res.json({ message: "Something error!" })
-        }
+      let room = await Room.create({
+        room_name,
+        hotel_id: +hotel_id,
+        room_desc,
+        room_area,
+        room_price,
+        room_bed: +room_bed,
+        room_quantity: +room_quantity,
+        room_num_people: +room_num_people,
+        room_imgs: roomImgsUrl.join(),
+        room_services,
+      });
+      return res.status(201).json({
+        code: 201,
+        name: "Created",
+        message: "create room successfully!",
+        data: room,
+      });
+    } catch (error) {
+      console.log(error);
+      return res.json({ message: "Something error!" });
     }
+  }
 
-    async delete(req, res) {
-        let room_id = req.params.id
-        if (!room_id) return res.status(400).send('room id not found')
+  async delete(req, res) {
+    let room_id = req.params.id;
+    if (!room_id) return res.status(400).send("room id not found");
 
-        try {
-            let room = await Room.findByPk(room_id)
-            if (!room) return res.status(400).send('room not found')
+    try {
+      let room = await Room.findByPk(room_id);
+      if (!room) return res.status(400).send("room not found");
 
-            await room.destroy()
+      await room.destroy();
 
-            return res.status(204).json({ code: 204, name: "REMOVE_ROOM", message: 'successfully' })
-
-        } catch (error) {
-            console.log(error)
-            return res.status(400).send(error.message)
-        }
+      return res
+        .status(204)
+        .json({ code: 204, name: "REMOVE_ROOM", message: "successfully" });
+    } catch (error) {
+      console.log(error);
+      return res.status(400).send(error.message);
     }
+  }
 
-    async update(req, res) {
+  async update(req, res) {
+    let slideImgs = req.files;
+    let data = req.body;
 
-        let slideImgs = req.files
-        let data = req.body
+    try {
+      let room = req.room;
+      if (!room) return res.status(400).send("room not found");
 
-
-        try {
-            let room = req.room
-            if (!room) return res.status(400).send('room not found')
-
-            // upload img
-            let roomImgsUrl = []
-            if (slideImgs) {
-                for (let img of slideImgs) {
-                    let result = await uploadFile(img)
-                    roomImgsUrl.push(result.key)
-                }
-
-            }
-
-            if (roomImgsUrl.length > 0) data.room_imgs = roomImgsUrl.join()
-            await room.update(data)
-
-            return res.status(204).send('update room successfully')
-        } catch (error) {
-            console.log(error)
-            return res.status(400).send("Something error!")
+      // upload img
+      let roomImgsUrl = [];
+      if (slideImgs) {
+        for (let img of slideImgs) {
+          let result = await uploadFile(img);
+          roomImgsUrl.push(result.key);
         }
+      }
+
+      if (roomImgsUrl.length > 0) data.room_imgs = roomImgsUrl.join();
+      await room.update(data);
+
+      return res.status(204).send("update room successfully");
+    } catch (error) {
+      console.log(error);
+      return res.status(400).send("Something error!");
     }
+  }
 }
 
-const roomController = new RoomController
-module.exports = roomController
+const roomController = new RoomController();
+module.exports = roomController;
--- routes/room.js
+++ routes/room.js
@@ -1,15 +1,32 @@
-const express = require("express")
-const roomRoute = express.Router()
-const roomController = require("../controllers/room")
-const authMiddleware = require('../middleware/auth')
-const { checkUpdateRoomPermission } = require('../middleware/room')
-const multer = require('multer')
-const upload = multer({ dest: 'uploads/' })
+const express = require("express");
+const roomRoute = express.Router();
+const roomController = require("../controllers/room");
+const authMiddleware = require("../middleware/auth");
+const { checkUpdateRoomPermission } = require("../middleware/room");
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
 
-roomRoute.get("/", roomController.index)
-roomRoute.get('/:id(\\d+$)', roomController.getById)
-roomRoute.post("/", authMiddleware.checkToken, upload.array('slideImgs'), authMiddleware.checkOwnerOfHotel, roomController.create)
-roomRoute.delete("/:id(\\d+$)", authMiddleware.checkToken, authMiddleware.checkOwnerOfHotel, roomController.delete)
-roomRoute.patch('/:id(\\d+$)', authMiddleware.checkToken, upload.array('slideImgs'), checkUpdateRoomPermission, roomController.update)
+roomRoute.get("/", roomController.index);
+roomRoute.get("/:id(\\d+$)", roomController.getById);
+roomRoute.post(
+  "/",
+  authMiddleware.checkToken,
+  upload.array("room_imgs"),
+  authMiddleware.checkOwnerOfHotel,
+  roomController.create
+);
+roomRoute.delete(
+  "/:id(\\d+$)",
+  authMiddleware.checkToken,
+  authMiddleware.checkOwnerOfHotel,
+  roomController.delete
+);
+roomRoute.patch(
+  "/:id(\\d+$)",
+  authMiddleware.checkToken,
+  upload.array("slideImgs"),
+  checkUpdateRoomPermission,
+  roomController.update
+);
 
-module.exports = roomRoute
+module.exports = roomRoute;
